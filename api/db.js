7de013c00f840bbc51b7f19b90cc27e131617726
ya95123 <ya95123@gmail.com>
2020-06-09 21:38:02 +0800

    API DB設定

---
+++ api/db.js
@@ -0,0 +1,45 @@
+// 引入
+import mongoose from 'mongoose'
+import beautifyUnique from 'mongoose-beautiful-unique-validation'
+// import validator from 'validator'
+
+const Schema = mongoose.Schema
+
+mongoose.set('useCreatIndex',true)
+// 連線本機的 shop 資料庫
+mongoose.connect('mongodb://127.0.0.1:27017/product', { useNewUrlParser: true, useUnifiedTopology: true })
+// 使用
+mongoose.plugin(beautifyUnique)
+
+const productSchema = new Schema({
+  name:{
+    type: String,
+    required:[true,'商品名稱必填'],
+    minlength:[1,'商品名稱至少輸入1個字'],
+    maxlength:[12,'商品名稱最多輸入12個字']
+  },
+  price:{
+    type: Number,
+    required:[true,'商品價格必填'],
+  },
+  // 商品說明
+  description:{
+    type: String
+  },
+  // 商品圖片
+  images:{
+    type: String
+  },
+  // 商品庫存
+  count:{
+    type:Number,
+    required:[true,'庫存數量必填'],
+  }
+})
+
+const product = mongoose.model('products',productSchema)
+
+// 預設匯出
+export default{
+  product
+}
